21adb6f2f17ba565694f0f8c0ba1049e88b367cd
rcl1 <pancakes21f@gmail.com>
2023-12-29 05:54:04 +0700

    Update build.gradle.kts
    Added 6 services to gradle: 3 from Firebase and 3 from Google

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,4 +44,20 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Firebase Authentication
+    implementation("com.google.firebase:firebase-auth:22.3.0")
+    // Firebase Cloud Firestore
+    implementation("com.google.firebase:firebase-firestore:24.10.0")
+    // Firebase Cloud Messaging (FCM)
+    implementation("com.google.firebase:firebase-messaging:23.4.0")
+
+    // Material Design
+    implementation("com.google.android.material:material:1.4.0")
+
+    // Google Maps
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
+
+    // Google Location
+    implementation("com.google.android.gms:play-services-location:21.0.1")
 }
