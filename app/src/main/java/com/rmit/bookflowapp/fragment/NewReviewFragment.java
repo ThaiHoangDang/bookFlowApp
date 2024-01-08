17dc5dbd865584fdac6a7c2dd44736b04bef1916
KoPhaiHoang <thaihoangdang1803@gmail.com>
2024-01-08 10:54:03 +0700

    add UI of NewReviewFragment

--- app/src/main/java/com/rmit/bookflowapp/fragment/NewReviewFragment.java
+++ app/src/main/java/com/rmit/bookflowapp/fragment/NewReviewFragment.java
@@ -4,34 +4,22 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.navigation.Navigation;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
-import com.rmit.bookflowapp.Model.Post;
-import com.rmit.bookflowapp.R;
 import com.rmit.bookflowapp.activity.MainActivity;
-import com.rmit.bookflowapp.adapter.PostAdapter;
-import com.rmit.bookflowapp.databinding.FragmentCreatePostBinding;
-import com.rmit.bookflowapp.databinding.FragmentHomeBinding;
-import com.rmit.bookflowapp.util.TranslateAnimationUtil;
+import com.rmit.bookflowapp.databinding.FragmentNewReviewBinding;
 
-import java.util.ArrayList;
+public class NewReviewFragment extends Fragment {
 
-public class CreatePostFragment extends Fragment {
-
-    private static final String TAG = "HomeFragment";
-    private FragmentCreatePostBinding bind;
+    private static final String TAG = "NewReviewFragment";
+    private FragmentNewReviewBinding bind;
     private MainActivity activity;
-    private PostAdapter postAdapter;
-    private ArrayList<Post> posts = new ArrayList<>();
 
-    public CreatePostFragment() {
+    public NewReviewFragment() {
         // Required empty public constructor
     }
 
@@ -45,7 +33,7 @@ public class CreatePostFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         activity = (MainActivity) getActivity();
-        bind = FragmentCreatePostBinding.inflate(inflater, container, false);
+        bind = FragmentNewReviewBinding.inflate(inflater, container, false);
         activity.setBottomNavigationBarVisibility(true);
 
         return bind.getRoot();
