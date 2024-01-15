36f19fb2df3645d6052634d319180957eb0887e3
Quoc Huynh <letrinhquoch@gmail.com>
2024-01-15 14:57:20 +0700

    added more fragment

---
+++ app/src/main/java/com/rmit/bookflowapp/fragment/UserProfileFragment.java
@@ -0,0 +1,73 @@
+package com.rmit.bookflowapp.fragment;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.rmit.bookflowapp.Model.User;
+import com.rmit.bookflowapp.R;
+import com.rmit.bookflowapp.activity.MainActivity;
+import com.rmit.bookflowapp.databinding.FragmentMoreBinding;
+import com.rmit.bookflowapp.databinding.FragmentUserProfileBinding;
+import com.rmit.bookflowapp.repository.UserRepository;
+import com.squareup.picasso.Picasso;
+
+public class UserProfileFragment extends Fragment {
+    private static final String TAG = "UserProfileFragment";
+    private FragmentUserProfileBinding binding;
+    private MainActivity activity;
+    private User user;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentUserProfileBinding.inflate(inflater, container, false);
+        activity = (MainActivity) getActivity();
+        activity.setBottomNavigationBarVisibility(true);
+
+        binding.backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                activity.navController.navigateUp();
+            }
+        });
+
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        UserRepository.getInstance().getUserById(firebaseUser.getUid()).addOnCompleteListener(new OnCompleteListener<User>() {
+            @Override
+            public void onComplete(@NonNull Task<User> task) {
+                user = task.getResult();
+                initView();
+            }
+        });
+        return binding.getRoot();
+    }
+
+    public void initView(){
+        if (user.getImageUrl() != null && !user.getImageUrl().isEmpty()) {
+            Picasso.get().load(user.getImageUrl()).into(binding.imageView);
+            Log.d(TAG, user.getImageUrl());
+        } else {
+            Picasso.get().load(FirebaseAuth.getInstance().getCurrentUser().getPhotoUrl().toString()).into(binding.imageView);
+            Log.d(TAG, FirebaseAuth.getInstance().getCurrentUser().getPhotoUrl().toString());
+        }
+        binding.textViewDisplayName.setText(user.getName());
+        binding.textViewEmail.setText(user.getEmail());
+        binding.textViewRole.setText(user.getRole());
+    }
+}
